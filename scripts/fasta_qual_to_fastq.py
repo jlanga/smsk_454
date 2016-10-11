aea5e4897f3675ec10829ace8a60a53c475edb10
Jorge Langa <jorge.langa.arranz@gmail.com>
2016-10-11 15:55:14 +0200

    Substituted trimmomatic > seqclean into snowhite

--- scripts/fasta_qual_to_fastq.py
+++ scripts/fasta_qual_to_fastq.py
@@ -5,17 +5,20 @@ from Bio.SeqIO.QualityIO import PairedFastaQualIterator
 import sys
 
 def convert(fasta, qual, fastq):
-    [SeqIO.write(record, fastq, "fastq") 
-        for record in PairedFastaQualIterator(fasta, qual)]
+    SeqIO.write(
+        record for record in PairedFastaQualIterator(fasta, qual),
+        fastq,
+        "fastq"
+    )
+
 
 if __name__ == "__main__":
-    
+
     if len(sys.argv) != 3:
         sys.exit("ERROR! Provide a fasta and a qual file. Output will be spitted through stdout")
-    
+
     fasta = open(sys.argv[1], "r")
     qual  = open(sys.argv[2], "r")
     fastq = sys.stdout
-    
-    convert(fasta, qual, fastq)
 
+    convert(fasta, qual, fastq)
--- scripts/install/from_tarball.sh
+++ scripts/install/from_tarball.sh
@@ -4,9 +4,15 @@ set -euo pipefail # Unoficial bash strict modern
 
 mkdir -p src/
 pushd src/
+
+# wget \
+#     --output-document seqclean-x86_64.tgz \
+#     --continue \
+#     http://downloads.sourceforge.net/project/seqclean/seqclean-x86_64.tgz
+# tar xvf seqclean-x86_64.tgz
+# popd
+
+
 wget \
-    --output-document seqclean-x86_64.tgz \
-    --continue \
-    http://downloads.sourceforge.net/project/seqclean/seqclean-x86_64.tgz
-tar xvf seqclean-x86_64.tgz
-popd
+    --output-document SnoWhite_2.0.3_release.tar.gz \
+    --continue http://dlugoschlab.arizona.edu/software/SnoWhite_2.0.3_release.tar.gz
